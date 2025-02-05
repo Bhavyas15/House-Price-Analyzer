67adba33f276b2f897de1d63cc073abe490fca0d
Bhavyas15 <139043257+Bhavyas15@users.noreply.github.com>
2025-02-05 20:17:20 +0530

    pep8 Update main.py

--- src/main.py
+++ src/main.py
@@ -5,26 +5,42 @@ from process_data import load_data, preprocess_data, train_model, process_onehot
 from collect_input import collect_info
 from EDA import show
 
-option = st.sidebar.radio('Select an option:', ['Price Prediction', 'Exploratory Data Analysis', 'Information'])
+# Sidebar
+option = st.sidebar.radio(
+    'Select an option:', 
+    ['Price Prediction', 'Exploratory Data Analysis', 'Information']
+)
 
+# Load and process main datasets
 df,df_test=load_data()
 df=preprocess_data(df)
+df_test=preprocess_data(df_test)
+
+st.title('House Price Prediction 🏡')
 
 if option=='Price Prediction':
-    df_test=preprocess_data(df_test)
+    # Normalization and one-hot encoding
     concat_df,concat_arr=process_onehot_norm(df,df_test)
 
+    # Train model
     train_model(concat_df)
-    st.title('House Price Prediction 🏡')
+
+    # Collect info
     output=collect_info(df)
-    st.header(f'Predicted Price : $ {np.round(output[0])}')
+
+    # Button to display predicted price
+    if st.button('Predict Price'):
+        st.header(f'Predicted Price : $ {np.round(output[0])}')
 
 elif option=='Information':
+    # Display info about parameters
     st.header('Information of Parameters \n\n')
+
     with open("data/data_description.txt", "r") as file:
-        content=file.read()
+        content=file.read() 
     st.write(content)
 
 else:
+    # Show EDA
     show(df)
     
