bd3962f49c3c4a544c90ffc0067915cd7457d3ae
Bhavyas15 <139043257+Bhavyas15@users.noreply.github.com>
2025-02-10 18:14:29 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,46 +1,16 @@
 import streamlit as st
-import numpy as np
+import pandas as pd
+from process_data import get_processed_data
+import UI
+from process_data import process_onehot_norm, train_model
+# Load processed data
+df,df_test = get_processed_data()
 
-from process_data import load_data, preprocess_data, train_model, process_onehot_norm
-from collect_input import collect_info
-from EDA import show
+# Normalization and one-hot encoding
+concat_df= process_onehot_norm(df, df_test)
 
-# Sidebar
-option = st.sidebar.radio(
-    'Select an option:', 
-    ['Price Prediction', 'Exploratory Data Analysis', 'Information']
-)
+# Train model
+train_model(concat_df)
 
-# Load and process main datasets
-df,df_test=load_data()
-df=preprocess_data(df)
-df_test=preprocess_data(df_test)
-
-st.title('House Price Prediction 🏡')
-
-if option=='Price Prediction':
-    # Normalization and one-hot encoding
-    concat_df,concat_arr=process_onehot_norm(df,df_test)
-
-    # Train model
-    train_model(concat_df)
-
-    # Collect info
-    output=collect_info(df)
-
-    # Button to display predicted price
-    if st.button('Predict Price'):
-        st.header(f'Predicted Price : $ {np.round(output[0])}')
-
-elif option=='Information':
-    # Display info about parameters
-    st.header('Information of Parameters \n\n')
-
-    with open("data/data_description.txt", "r") as file:
-        content=file.read() 
-    st.write(content)
-
-else:
-    # Show EDA
-    show(df)
-    
+# UI interface
+UI.run_ui(df)
